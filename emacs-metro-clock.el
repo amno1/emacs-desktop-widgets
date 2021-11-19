9d8c172a043e51c068bfa0ead434f5b5f5fd0375
Arthur Miller <arthur.miller@live.com>
2021-11-19 13:05:44 +0100

    Metro and Vision clocks tests.

---
+++ emacs-metro-clock.el
@@ -0,0 +1,147 @@
+;;; emacs-metro-clock.el ---  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Arthur Miller
+
+;; Author: Arthur Miller <arthur.miller@live.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; https://www.deviantart.com/satya164/art/Conky-Metro-Clock-245432929
+
+;;; Code:
+(require 'time-stamp)
+
+(defgroup emacs-metro-clock nil
+  "Conky line clock and weather monitoring application written in Emacs Lisp."
+  :prefix "emc--"
+  :prefix "emacs-metro-clock-"
+  :tag "Conky metro style clock for Emacs"
+  :group 'applications)
+
+;;; Customize
+(defcustom emacs-metro-clock-x -200
+  "Frame width"
+  :type 'fixnum
+  :group 'emacs-metro-clock)
+
+(defcustom emacs-metro-clock-y 600
+  "Frame width"
+  :type 'fixnum
+  :group 'emacs-metro-clock)
+
+(defface emacs-metro-clock-day-face
+  '((t :weight light :family "Open Sans" :default nil
+       :foreground "white" :height 400))
+  "Default face for emacs-metro-clock frame."
+  :group 'emacs-metro-clock)
+
+(defface emacs-metro-clock-month-face
+  '((t :weight light :family "Open Sans" :default nil
+       :foreground "white" :height 400))
+  "Default face for emacs-metro-clock frame."
+  :group 'emacs-metro-clock)
+
+(defface emacs-metro-clock-time-face
+  '((t :weight normal :family "Open Sans" :default nil
+       :foreground "white" :height 1000))
+  "Default face for emacs-metro-clock frame."
+  :group 'emacs-metro-clock)
+
+;;; Implementation
+(defvar emc--frame nil)
+(defvar emc--timer nil)
+(defvar emc--buffer nil)
+(defvar emc--day-face 'emacs-metro-clock-day-face)
+(defvar emc--time-face 'emacs-metro-clock-time-face)
+(defvar emc--month-face 'emacs-metro-clock-month-face)
+
+(defun emc--time ()
+  (propertize (time-stamp-string "%H:%M") 'face emc--time-face))
+
+(defun emc--day ()
+  (propertize (capitalize (time-stamp-string "%A")) 'face emc--day-face))
+
+(defun emc--month ()
+  (propertize (capitalize (time-stamp-string "%B %d")) 'face emc--month-face))
+
+(defun emc--make-frame ()
+  (make-frame `((name . "*emacs-metro-clock*")
+                (left . ,emacs-metro-clock-x)
+                (top . ,emacs-metro-clock-y)
+                (alpha-background . 0)
+                (tool-bar-lines . 0)
+                (menu-bar-lines . 0)
+                (left-fringe . 0)
+                (right-fringe . 0)
+                (minibuffer . nil)
+                (horizontal-scroll-bars . nil)
+                (vertical-scroll-bars . nil)
+                (border-width . 0)
+                (internal-border-width . 0)
+                (visibility . nil)
+                (z-group . below)
+                (auto-raise . nil)
+                (skip-taskbar . t)
+                (no-focus-on-map . t)
+                (no-accept-focus . t)
+                (unsplittable . t)
+                (undecorated . t))))
+
+(defun emc--update ()
+  (select-frame emc--frame)
+  (with-current-buffer emc--buffer
+    (goto-char (point-min))
+    (re-search-forward "[0-9][0-9]:[0-9][0-9]" nil t)
+    (replace-match (emc--time))))
+
+(defun emc--make-buffer ()
+  (with-current-buffer (get-buffer-create "  *emacs-metro-clock*")
+    (setq cursor-in-non-selected-windows nil mode-line-format nil)
+    (erase-buffer)
+    (insert (emc--day) "\n" (emc--month) "\n" (emc--time))
+    (let ((fill-column (- (line-end-position) (line-beginning-position))))
+      (goto-char (point-min))
+      (center-line))
+    (setq emc--buffer (current-buffer))))
+
+(defun emc--make-widget ()
+  (let ((frame (emc--make-frame)))
+    (emc--make-buffer)
+    (select-frame frame)
+    (switch-to-buffer emc--buffer)
+    (delete-other-windows)
+    (fit-frame-to-buffer)
+    (set-frame-width emc--frame (1+ (frame-width)))
+    (make-frame-visible frame)
+    (set-window-dedicated-p (selected-window) t)
+    (setq emc--frame frame emc--timer (run-at-time nil 60 #'emc--update))))
+
+;;; Public commands
+(defun emacs-metro-clock-run ()
+  (interactive)
+  (unless emc--frame
+    (emc--make-widget)))
+
+(defun emacs-metro-clock-stop ()
+  (interactive)
+  (if emc--timer (cancel-timer emc--timer))
+  (if emc--frame (delete-frame emc--frame))
+  (if emc--buffer (kill-buffer emc--buffer))
+  (setq emc--buffer nil emc--frame nil emc--timer nil))
+
+(provide 'emacs-metro-clock)
+;;; emacs-metro-clock.el ends here
---
+++ emacs-vision-clock.el
@@ -0,0 +1,147 @@
+;;; emacs-vision-clock.el ---  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Arthur Miller
+
+;; Author: Arthur Miller <arthur.miller@live.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; https://github.com/BobbyBakes/conky-Vision
+
+;;; Code:
+(require 'time-stamp)
+
+(defgroup emacs-vision-clock nil
+  "Conky line clock and weather monitoring application written in Emacs Lisp."
+  :prefix "evc--"
+  :prefix "emacs-vision-clock-"
+  :tag "Conky-Vision style clock as Emacs application."
+  :group 'applications)
+
+;;; Customize
+(defcustom emacs-vision-clock-x -100
+  "Frame width"
+  :type 'fixnum
+  :group 'emacs-vision-clock)
+
+(defcustom emacs-vision-clock-y 100
+  "Frame width"
+  :type 'fixnum
+  :group 'emacs-vision-clock)
+
+(defface emacs-vision-clock-day-face
+  '((t :weight light :family "Poiret One" :default nil
+       :foreground "white" :height 340))
+  "Default face for emacs-vision-clock frame."
+  :group 'emacs-vision-clock)
+
+(defface emacs-vision-clock-month-face
+  '((t :weight light :family "Poiret One" :default nil
+       :foreground "white" :height 340))
+  "Default face for emacs-vision-clock frame."
+  :group 'emacs-vision-clock)
+
+(defface emacs-vision-clock-time-face
+  '((t :weight bold :family "Poiret One" :default nil
+       :foreground "white" :height 900))
+  "Default face for emacs-vision-clock frame."
+  :group 'emacs-vision-clock)
+
+;;; Implementation
+(defvar evc--frame nil)
+(defvar evc--timer nil)
+(defvar evc--buffer nil)
+(defvar evc--day-face 'emacs-vision-clock-day-face)
+(defvar evc--time-face 'emacs-vision-clock-time-face)
+(defvar evc--month-face 'emacs-vision-clock-month-face)
+
+(defun evc--time ()
+  (propertize (time-stamp-string "%H:%M") 'face evc--time-face))
+
+(defun evc--day ()
+  (propertize (capitalize (time-stamp-string "%A")) 'face evc--day-face))
+
+(defun evc--month ()
+  (propertize (capitalize (time-stamp-string "%B %d")) 'face evc--month-face))
+
+(defun evc--make-frame ()
+  (make-frame `((name . "*emacs-vision-clock*")
+                (left . ,emacs-vision-clock-x)
+                (top . ,emacs-vision-clock-y)
+                (alpha-background . 0)
+                (tool-bar-lines . 0)
+                (menu-bar-lines . 0)
+                (minibuffer . nil)
+                (right-fringe . 0)
+                (left-fringe . 0)
+                (horizontal-scroll-bars . nil)
+                (vertical-scroll-bars . nil)
+                (border-width . 0)
+                (internal-border-width . 0)
+                (visibility . nil)
+                (z-group . below)
+                (auto-raise . nil)
+                (skip-taskbar . t)
+                (no-focus-on-map . t)
+                (no-accept-focus . t)
+                (unsplittable . t)
+                (undecorated . t))))
+
+(defun evc--update ()
+  (select-frame evc--frame)
+  (with-current-buffer evc--buffer
+    (goto-char (point-min))
+    (re-search-forward "[0-9][0-9]:[0-9][0-9]" nil t)
+    (replace-match (evc--time))))
+
+(defun evc--make-buffer ()
+  (with-current-buffer (get-buffer-create "  *emacs-vision-clock*")
+    (setq cursor-in-non-selected-windows nil mode-line-format nil)
+    (erase-buffer)
+    (insert (evc--time) "\n " (evc--day) ". " (evc--month))
+    (let ((fill-column (- (line-end-position) (line-beginning-position))))
+      (goto-char (point-min))
+      (center-line))
+    (setq evc--buffer (current-buffer))))
+
+(defun evc--make-widget ()
+  (let ((frame (evc--make-frame)))
+    (evc--make-buffer)
+    (select-frame frame)
+    (switch-to-buffer evc--buffer)
+    (delete-other-windows)
+    (fit-frame-to-buffer)
+    (set-frame-width evc--frame (1+ (frame-width)))
+    (make-frame-visible frame)
+    (set-window-dedicated-p (selected-window) t)
+    (setq evc--frame frame evc--timer (run-at-time nil 60 #'evc--update))))
+
+;;; Public commands
+(defun emacs-vision-clock-run ()
+  (interactive)
+  (unless evc--frame
+    (evc--make-widget)))
+
+(defun emacs-vision-clock-stop ()
+  (interactive)
+  (if evc--timer (cancel-timer evc--timer))
+  (if evc--frame (delete-frame evc--frame))
+  (if evc--buffer (kill-buffer evc--buffer))
+  (setq evc--buffer nil evc--frame nil evc--timer nil))
+
+(provide 'emacs-vision-clock)
+;;; emacs-vision-clock.el ends here
